dc63c1a6ae6819841b89f9d8f23e6d64945a77e4
DanielHabenicht <daniel-habenicht@outlook.de>
2023-05-21 23:39:18 +0200

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -15,9 +15,13 @@ benchmark.add_local_methods(methods)
 benchmark.run_complexity_analysis(
     methods=methods,
     style="time",
+    n_max=181,
+    n_measures=20,
 )
 
 benchmark.run_complexity_analysis(
     methods=methods,
     style="junctions",
+    n_max=181,
+    n_measures=20,
 )
