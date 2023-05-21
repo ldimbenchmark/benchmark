6af40d997aa4b5fbcb0ff0904dfb0556546e08b2
DanielHabenicht <daniel-habenicht@outlook.de>
2023-05-21 17:16:28 +0200

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,8 +1,11 @@
 from ldimbenchmark.methods import LILA, MNF, DUALMethod
 from ldimbenchmark import LDIMBenchmark
 
-methods = [MNF(), LILA(), DUALMethod()]
-
+methods = [
+    "ghcr.io/ldimbenchmark/dualmethod:0.2.2",
+    "ghcr.io/ldimbenchmark/lila:0.2.2",
+    "ghcr.io/ldimbenchmark/mnf:0.2.2",
+]
 
 benchmark = LDIMBenchmark(
     {"lila": {"default_flow_sensor": "sum"}}, [], results_dir="./benchmark-results"
