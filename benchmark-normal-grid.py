61692bbbd9b7c81a21be7eceedf8a2e8e111704c
Daniel Habenicht <daniel-habenicht@outlook.de>
2023-06-24 15:33:55 +0200

    add better intervals

--- benchmark-normal-grid.py
+++ benchmark-normal-grid.py
@@ -26,16 +26,19 @@ if __name__ == "__main__":
     param_grid = {
         "lila": {
             "battledim": {
-                "est_length": np.arange(24, 24 * 8, 24).tolist(), # Optimal: Battledim: 168, Graz: x, Gjovik: x
-                "C_threshold": np.arange(2, 16, 1).tolist(), # Optimal: Battledim: 15, Graz: x, Gjovik: x
-                "delta": np.arange(4, 14, 1).tolist(), # Optimal: Battledim: 12, Graz: x, Gjovik: x
+                "est_length": np.arange(24, 24 * 12, 24).tolist(), # Optimal: Battledim: 168, Graz: x, Gjovik: x
+                "C_threshold": np.arange(2, 25, 1).tolist(), # Optimal: Battledim: 15, Graz: x, Gjovik: x
+                "delta": np.arange(4, 25, 1).tolist(), # Optimal: Battledim: 12, Graz: x, Gjovik: x
                 "default_flow_sensor": ["sum"],
-                "resample_frequency": ["60T", "30T", "5T", "1T"], # Optimal: Battledim: 5T, Graz: x, Gjovik: 
+                "resample_frequency": ["5T"], # ["60T", "30T", "10T", "5T", "1T"], # Optimal: Battledim: 5T, Graz: x, Gjovik: 
+                # "dma_specific": [True]
             },
             "graz-ragnitz": {
-                "est_length": np.arange(24, 24 * 8, 24).tolist(), 
-                "C_threshold": np.arange(2, 16, 1).tolist(),
-                "delta": np.arange(4, 14, 1).tolist(),
+                "leakfree_time_start": ["2016-04-12 01:15:00"],
+                "leakfree_time_stop": ["2016-04-12 01:29:59"],
+                "est_length": np.around(np.linspace(0.1, 1.7, 17),1).tolist(), 
+                "C_threshold": np.arange(1, 20, 1).tolist(),
+                "delta": np.arange(-5, -2, 1).tolist() + np.around(np.linspace(-2, 1, 16),1).tolist() + np.arange(1, 5, 1).tolist(),
                 "default_flow_sensor": ["wNode_1"],
                 "resample_frequency": ["5T", "1T", "30s", "20s", "15s", "10s", "5s"], 
             }
@@ -43,7 +46,7 @@ if __name__ == "__main__":
         "mnf": {
             "battledim": {
                 # "gamma": np.arange(-10, 10, 1).tolist(),
-                "gamma": np.arange(-0.3, 1, 0.05).tolist(), # Optimal: Battledim: 1.0, Graz: x, Gjovik: x
+                "gamma": np.around(np.arange(-0.3, 1.0, 0.05),2).tolist(), # Optimal: Battledim: 1.0, Graz: x, Gjovik: x
                 "window": [1, 5, 10, 20], # Optimal: Battledim: 10, Graz: x, Gjovik: x
                 # "resample_frequency": ["60T"], # Optimal: Battledim: 5T, Graz: x, Gjovik: x
             },
@@ -56,20 +59,21 @@ if __name__ == "__main__":
         "dualmethod": {
             "battledim": {
                 "est_length": np.arange(24, 24 * 40, 48).tolist(), # Optimal: Battledim: 888, Graz: x, Gjovik: x
-                "C_threshold": np.arange(0, 1, 0.2).tolist() + np.arange(2, 6, 1).tolist(), # Optimal: Battledim: 0.2, Graz: x, Gjovik: x
-                "delta": np.arange(0, 4, 0.2).tolist() + np.arange(2, 6, 1).tolist(), # Optimal: Battledim: 4, Graz: x, Gjovik: x
+                "C_threshold": np.around(np.arange(0, 1, 0.2),1).tolist() + np.arange(2, 6, 1).tolist(), # Optimal: Battledim: 0.2, Graz: x, Gjovik: x
+                "delta": np.around(np.arange(0, 4, 0.2)).tolist() + np.arange(2, 6, 1).tolist(), # Optimal: Battledim: 4, Graz: x, Gjovik: x
             },
             "graz-ragnitz": {
-                 "est_length": np.arange(24, 24 * 40, 48).tolist(), # Optimal: Battledim: 888, Graz: x, Gjovik: x
-                "C_threshold": np.arange(0, 1, 0.2).tolist() + np.arange(2, 6, 1).tolist(), # Optimal: Battledim: 0.2, Graz: x, Gjovik: x
-                "delta": np.arange(0, 4, 0.2).tolist() + np.arange(2, 6, 1).tolist(), # Optimal: Battledim: 4, Graz: x, Gjovik: x
+                "resample_frequency": ["1T"], 
+                "est_length": np.around(np.linspace(0.1, 2, 20),1).tolist(),
+                "C_threshold": np.around(np.linspace(0, 2, 20),1).tolist(),
+                "delta": np.around(np.linspace(-3, 3, 18),1).tolist(),
             }
         },
     }
 
     datasets = [
         # Dataset("./datasets/gjovik"),
-        Dataset("./datasets/graz-ragnitz"),
+        # Dataset("./datasets/graz-ragnitz"),
         Dataset("./datasets/battledim"),
     ]
 
@@ -81,21 +85,23 @@ if __name__ == "__main__":
         multi_parameters=True,
     )
 
-    benchmark.add_docker_methods(["ghcr.io/ldimbenchmark/mnf:1.2.0"])
+    # benchmark.add_docker_methods(["ghcr.io/ldimbenchmark/mnf:1.2.0"])
     benchmark.add_docker_methods(["ghcr.io/ldimbenchmark/lila:0.2.0"])
-    benchmark.add_docker_methods(["ghcr.io/ldimbenchmark/dualmethod:0.1.0"])
+    # benchmark.add_docker_methods(["ghcr.io/ldimbenchmark/dualmethod:0.1.0"])
 
-    # execute benchmark
+    # # execute benchmark
     benchmark.run_benchmark(
-        "training",
+        "evaluation",
         parallel=True,
-        # parallel_max_workers=8, 
-        memory_limit="10g",
+        parallel_max_workers=4, 
+        memory_limit="14g",
     )
 
     benchmark.evaluate(
-        write_results="db",
-        current_only=False,
+        write_results=["db"],
+        current_only=True,
         print_results=False
         # resultFilter=lambda results: results[results["F1"].notna()],
     )
+
+    # benchmark.evaluate_run("lila_0.2.0_graz-ragnitz-8f370b5676ecaad892ba6db24135b6af_evaluation_d995add1b1a1c944318687406fa0b452")
